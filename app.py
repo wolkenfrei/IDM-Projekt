1b71e47155b76945dcc233148c8b77f480502f83
wolkenfrei <78296366+wolkenfrei@users.noreply.github.com>
2026-03-15 13:11:02 +0100

    app.py

--- app.py
+++ app.py
@@ -10,14 +10,18 @@
 
 #Block 0 – Imports
 import streamlit as st
+import random #Zufallszahlen
+from datetime import datetime #Datum und Uhrzeit
 
 
 #Block 1 – Session State
 #Streamlit startet Script ständig neu (Rerun bei jeder Änderung)
 if "request_text" not in st.session_state:
     st.session_state.request_text = ""
+if "requests" not in st.session_state:
+    st.session_state.requests = [] #Liste mit den erstellten Requests
 if "request_ok" not in st.session_state:
-     st.session_state.request_ok = False #Bei True = Erfolgsmeldung anzeigen, bei False = keine Erfolgsmeldung
+    st.session_state.request_ok = False #Bei True = Erfolgsmeldung anzeigen, bei False = keine Erfolgsmeldung
 
 
 #Block 2 – Page Setup & Header
@@ -29,47 +33,55 @@ st.write("IDM Lernprojekt")#Kurzbeschreibung
 st.sidebar.title("Navigation")#Überschrift der Sidebar
 st.sidebar.link_button("Projekt auf GitHub ansehen", "https://github.com/wolkenfrei/IDM-Projekt")#Verweis auf GitHub für weitere Informationen
 
+
 #Block 3 – Navigation
-auswahl = st.sidebar.selectbox( #Navigation - steuert den Seiteninhalt
+auswahl = st.sidebar.selectbox(  # Navigation - steuert den Seiteninhalt
     "Bitte auswählen",
-    ["Antrag erstellen", "Antrag anzeigen"])
-#Anzeige abhängig von der Auswahl
+    ["Antrag erstellen", "Antrag anzeigen"]
+)
+
+# Anzeige abhängig von der Auswahl
 if auswahl == "Antrag erstellen":
     st.write("Bitte User-ID eingeben und Request-Typ auswählen.")
-else:
-    st.write("Hier kommt später eine Liste.")
 
+    #Block 4 – Inputs / Variablen (UI - Werte)
+    user_id = st.text_input("User-ID")  # Pflichtfeld der ID
+    request_type = st.selectbox(
+        "Request-Typ",
+        ["Einzelberechtigung", "Neuer User (Onboarding)"]
+    )  # Logikpfad der Anwendung
 
-#Block 4 – Inputs / Variablen (UI - Werte)
-user_id = st.text_input("User-ID")#Pflichtfeld der ID
-request_type = st.selectbox("Request-Typ", ["Einzelberechtigung", "Neuer User (Onboarding)"])#Logikpfad der Anwendung
-#Defaults verhindern NameError bei späteren Abfragen
-system = None #Startwert sonst crash (NameError)
-request_text = "" #sammelt Ausgabe für den Request Text
-#Checkbox-Defaults (existieren immer, auch wenn UI-Zweig nicht aktiv ist)
-ad = mail = servicenow = sap = False #boolean
-system_1 = system_2 = system_3 = system_4 = False #boolean
+    # Defaults verhindern NameError bei späteren Abfragen
+    system = None  # Startwert sonst crash (NameError)
+    request_text = ""  # sammelt Ausgabe für den Request Text
 
+    # Checkbox-Defaults (existieren immer, auch wenn UI-Zweig nicht aktiv ist)
+    ad = mail = servicenow = sap = False  # boolean
+    system_1 = system_2 = system_3 = system_4 = False  # boolean
 
-#Block 5 – Conditional UI (abhängig vom Request-Typ)
-if request_type == "Einzelberechtigung":#Logik und UI für Einzelberechtigungen
-    st.write("Wähle nun das gewünschte System aus")
-    system = st.selectbox("System", ["Bitte wählen...", "SAP", "AD", "ServiceNow", "Email"])#Bitte wählen ist Standard/Platzhalter
+    #Block 5 – Conditional UI (abhängig vom Request-Typ)
+    if request_type == "Einzelberechtigung":  # Logik und UI für Einzelberechtigungen
+        st.write("Wähle nun das gewünschte System aus")
+        system = st.selectbox(
+            "System",
+            ["Bitte wählen...", "SAP", "AD", "ServiceNow", "Email"]
+        )  # Bitte wählen ist Standard/Platzhalter
 
-    if system == "SAP":#Zusatzoption aber nur wenn SAP ausgewählt worden ist
-        st.write("System auswählen")
-        system_1 = st.checkbox("System_1")
-        system_2 = st.checkbox("System_2")
-        system_3 = st.checkbox("System_3")
-        system_4 = st.checkbox("System_4")
-    
-else:
-        st.write("Neuer User - wähle die gewünschten Berechtigungen aus")#Einzeln anwählbar
+        if system == "SAP":  # Zusatzoption aber nur wenn SAP ausgewählt worden ist
+            st.write("System auswählen")
+            system_1 = st.checkbox("System_1")
+            system_2 = st.checkbox("System_2")
+            system_3 = st.checkbox("System_3")
+            system_4 = st.checkbox("System_4")
+
+    else:
+        st.write("Neuer User - wähle die gewünschten Berechtigungen aus")  # Einzeln anwählbar
 
         ad = st.checkbox("AD / Benutzerkonto")
         mail = st.checkbox("E-Mail / Exchange")
         servicenow = st.checkbox("ServiceNow")
-        sap = st.checkbox("SAP")#Rollen/Optionen nur anzeigen, wenn SAP angehakt ist
+        sap = st.checkbox("SAP")  # Rollen/Optionen nur anzeigen, wenn SAP angehakt ist
+
         if sap:
             st.write("System auswählen")
             system_1 = st.checkbox("System_1")
@@ -77,54 +89,56 @@ else:
             system_3 = st.checkbox("System_3")
             system_4 = st.checkbox("System_4")
 
+    #Block 6 – Request (Button + Text erstellen)
+    button_geklickt = st.button("Request erstellen")  # Triggert die Request Generierung
 
-#Block 6 – Request (Button + Text erstellen)
-button_geklickt = st.button("Request erstellen")#Triggert die Request Generierung
+    if button_geklickt:
+        request_id = random.randint(1000, 9999)  # Erzeugt eine zufällige Request-ID
+        timestamp = datetime.now().strftime("%d.%m.%y - %H:%M Uhr")  # aktuelles Datum und Uhrzeit
+        request_text = f"Request-ID: {request_id}\nDatum: {timestamp}\nUser-ID: {user_id}\n"
 
-if button_geklickt:
-    request_text = f"User-ID: {user_id}\n" #Request Text wird nur beim Button Klick erzeugt
+        if user_id == "":
+            st.warning("Bitte User-ID eingeben")  # Schutz gegen leeres Pflichtfeld - Gelbe Warnmeldung
+            st.session_state.request_ok = False  # Erfolgsmeldung deaktivieren
+            st.session_state.request_text = ""  # Optional den Text leeren
+            st.stop()  # Stop, hier stoppt der Code
 
-    if user_id == "":
-        st.warning("Bitte User-ID eingeben") #Schutz gegen leeres Pflichtfeld - Gelbe Warnmeldung
-        st.session_state.request_ok = False #Erfolgsmeldung deaktivieren
-        
-    else:
-       
-        if request_type == "Einzelberechtigung": #seperater Flow - genau ein System
-
+        if request_type == "Einzelberechtigung":  # separater Flow - genau ein System
             if system == "Bitte wählen...":
-                 st.error("Bitte System auswählen")#Rote Meldung
-                 st.session_state.request_ok = False#Kein Erfolg
-                 st.session_state.request_text = ""#Optional den Text leeren
-                 st.stop()#Stop, hier stopt der Code
-            
-            else:
-                 request_text += "Typ: Einzelberechtigung\n"
-                 request_text += f"System: {system}\n"
+                st.error("Bitte System auswählen")  # Rote Meldung
+                st.session_state.request_ok = False  # Kein Erfolg
+                st.session_state.request_text = ""  # Optional den Text leeren
+                st.stop()  # Stop, hier stoppt der Code
 
-                 if system == "SAP":
-                    request_text += "SAP Systeme:\n"
-                    if system_1:
-                        request_text += "- System_1\n"
-                    if system_2:
-                        request_text += "- System_2\n"
-                    if system_3:
-                        request_text += "- System_3\n"
-                    if system_4:
-                        request_text += "- System_4\n"
-                    if not (system_1 or system_2 or system_3 or system_4):
-                         st.error("Bitte mindestens ein SAP-System auswählen")#Rote Meldung
-                         st.session_state.request_ok = False#Kein Erfolg
-                         st.stop()#Stop, hier stopt der Code             
+            request_text += "Typ: Einzelberechtigung\n"
+            request_text += f"System: {system}\n"
+
+            if system == "SAP":
+                if not (system_1 or system_2 or system_3 or system_4):
+                    st.error("Bitte mindestens ein SAP-System auswählen")  # Rote Meldung
+                    st.session_state.request_ok = False  # Kein Erfolg
+                    st.session_state.request_text = ""  # Optional den Text leeren
+                    st.stop()  # Stop, hier stoppt der Code
+
+                request_text += "SAP Systeme:\n"
+                if system_1:
+                    request_text += "- System_1\n"
+                if system_2:
+                    request_text += "- System_2\n"
+                if system_3:
+                    request_text += "- System_3\n"
+                if system_4:
+                    request_text += "- System_4\n"
 
         else:
-            request_text += "Typ: Neuer User (Onboarding)\n" #onboarding Flow - mehrere Systeme gleichzeitig
+            request_text += "Typ: Neuer User (Onboarding)\n"  # Onboarding - mehrere Systeme gleichzeitig
             request_text += "Benötigte Systeme:\n"
-            if not(ad or mail or servicenow or sap):
-                 st.error("Bitte mindestens System auswählen")#Rote Meldung
-                 st.session_state.request_ok = False#Kein Erfolg
-                 st.session_state.request_text = ""#Optional den Text leeren
-                 st.stop()#Stop, hier stopt der Code
+
+            if not (ad or mail or servicenow or sap):
+                st.error("Bitte mindestens ein System auswählen")  # Rote Meldung
+                st.session_state.request_ok = False  # Kein Erfolg
+                st.session_state.request_text = ""  # Optional den Text leeren
+                st.stop()  # Stop, hier stoppt der Code
 
             if ad:
                 request_text += "- AD\n"
@@ -136,27 +150,40 @@ if button_geklickt:
                 request_text += "- SAP\n"
 
             if sap:
+                if not (system_1 or system_2 or system_3 or system_4):
+                    st.error("Bitte mindestens ein SAP-System auswählen")  # Rote Meldung
+                    st.session_state.request_ok = False  # Kein Erfolg
+                    st.session_state.request_text = ""  # Optional den Text leeren
+                    st.stop()  # Stop, hier stoppt der Code
+
                 request_text += "SAP Rollen:\n"
-                if system_1: 
-                        request_text += "- System_1\n"
+                if system_1:
+                    request_text += "- System_1\n"
                 if system_2:
-                        request_text += "- System_2\n"
+                    request_text += "- System_2\n"
                 if system_3:
-                        request_text += "- System_3\n"
+                    request_text += "- System_3\n"
                 if system_4:
                     request_text += "- System_4\n"
-                if not (system_1 or system_2 or system_3 or system_4):
-                         st.error("Bitte mindestens ein SAP-System auswählen")#Rote Meldung
-                         st.session_state.request_ok = False#Kein Erfolg
-                         st.stop()#Stop, hier stopt der Code
 
-        st.session_state.request_text = request_text #Speichert aktuell trotzdem, auch wenn keine ID hinterlegt ist - Muss noch gefixt werden!
-        st.session_state.request_ok = True #Erfolgsmeldung aktivieren
+        st.session_state.request_text = request_text  # Speichert den fertigen Request-Text
+        st.session_state.request_ok = True  # Erfolgsmeldung aktivieren
+        st.session_state.requests.append(request_text)  # Fügt den fertigen Text zur Liste hinzu
 
-#Block 7 – Output (Anzeige)
-if st.session_state.request_ok: #Wird nur angezeigt, wenn letzter Klick erfolgreich war
-     st.success("Request erfolgreich erstellt") #Grüne Meldung im UI
-st.text_area("Request Text", st.session_state.request_text, height=200) #Hier wird der erstellte Request angezeigt
+    #Block 7 – Output (Anzeige)
+    if st.session_state.request_ok:  # Wird nur angezeigt, wenn letzter Klick erfolgreich war
+        st.success("Request erfolgreich erstellt")  # Grüne Meldung im UI
+        st.text_area("Request Text", st.session_state.request_text, height=200)  # Hier wird der erstellte Request angezeigt
 
 
-    
+#Block 8 - Antrag unter "Antrag anzeigen"
+else:
+    st.write("Bisher erstellte Anträge")  # Bereich für die Request-Übersicht
+
+    if not st.session_state.requests:
+        st.info("Es wurden noch keine Anträge erstellt.")  # Liste ist leer - nur der Hinweis
+    else:
+        for request in st.session_state.requests:
+            st.text_area("Gespeicherter Antrag", request, height=200)  # Zeigt jeden Request an
+
+
